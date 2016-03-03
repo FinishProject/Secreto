9be86a9720207bec6f82ab3fe5c8c32a794d4e0a
LeeRang <asw77id@gmail.com>
2016-03-03 19:16:03 +0900

    03.03
    징검다리, 캐릭터 회전 수정
    가속도 추가

--- Assets/02. Script/Holds/Stage 1/JingGume.cs
+++ Assets/02. Script/Holds/Stage 1/JingGume.cs
@@ -4,45 +4,30 @@ using System.Collections;
 public class JingGume : MonoBehaviour {
 
     public GameObject[] gObject;
-    private Transform[] chTr;
-    private Transform playerTr;
+    private int cnt = 1;
 
-    private float distance;
-    private int cnt = 0;
+    JingGume parent;
 
-	void Start () {
-        chTr = GetComponentsInChildren<Transform>();
-        //발판 렌더링 끄기
+    void Start () {
+        //발판 끄기
         for (int i = 1; i < gObject.Length; i++)
         {
             gObject[i].SetActive(false);
         }
+        parent = transform.parent.GetComponentInParent<JingGume>();
 	}
+
     void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag == "Player")
+        if (col.gameObject.tag == "Player")
         {
-            playerTr = col.gameObject.transform;
+            parent.OnHolds();
         }
     }
 
-    void OnTriggerStay(Collider col)
+    void OnHolds()
     {
-        if (playerTr != null)
-        {
-            //플레이어와 발판간의 거리 체크
-            distance = Vector3.Distance(playerTr.position, chTr[cnt].position);
-            if (distance <= 1.9f)
-            {
-                gObject[cnt].SetActive(true);
-                if (cnt < gObject.Length - 1)
-                    cnt++;
-            }
-        }
-    }
-
-    void OnTriggerExit(Collider col)
-    {
-        playerTr = null;
+        if(cnt < gObject.Length)
+            gObject[cnt++].SetActive(true);
     }
 }
