39eab5f1b11dcd625dc7973ecd4f8985f81025cc
LeeRang <asw77id@gmail.com>
2016-03-04 21:55:16 +0900

    카메라 수정

--- Assets/02. Script/Player/CameraCtrl.cs
+++ Assets/02. Script/Player/CameraCtrl.cs
@@ -5,22 +5,18 @@ public class CameraCtrl : MonoBehaviour {
 
     public Transform playerTr; // 플레이어의 위치값
     public float speed = 10f; // 카메라의 속도
-    private float relCameraPosMag;
 
     private Vector3 relCameraPos;
-    private Vector3 checkPoint;
 
     void Start()
     {
-        relCameraPos = transform.position - playerTr.position;    
-    }
+        relCameraPos = transform.position - playerTr.position;
 
+    }
     void FixedUpdate()
     {
-        relCameraPosMag = relCameraPos.sqrMagnitude;
         Vector3 standardPos = playerTr.position + relCameraPos;
-        checkPoint = Vector3.Lerp(standardPos, playerTr.position, 0.25f);
-        transform.position = Vector3.Lerp(transform.position, checkPoint, speed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, standardPos, speed * Time.deltaTime);
 
     }
 
