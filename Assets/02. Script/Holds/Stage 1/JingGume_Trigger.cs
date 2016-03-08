9092676b1fc747c6faa076f40796cd63d0ca2aa9
LeeRang <asw77id@gmail.com>
2016-03-08 13:19:26 +0900

    합병
    백철승씨가 만든 코드 합병

---
+++ Assets/02. Script/Holds/Stage 1/JingGume_Trigger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JingGume_Trigger : MonoBehaviour {
+    
+    public  GameObject[] gObjects = null;
+    public  int[]        requisiteCnts = null;     // 필요 조건 (개수)
+    private int          Cnt = 0;
+
+    JingGume_Trigger parent;
+
+    void Start()
+    {
+        //발판 끄기
+        for (int i = 1; i < gObjects.Length; i++)
+        {
+            gObjects[i].SetActive(false);
+        }
+        if (transform.parent)
+            parent = transform.parent.GetComponentInParent<JingGume_Trigger>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Cnt++;
+            parent.OnHolds();
+        }
+    }
+
+    void OnHolds()
+    {
+        for (int i = 1; i < gObjects.Length; i++)
+        {
+            if(requisiteCnts[i] <= gObjects[i-1].GetComponent<JingGume_Trigger>().Cnt)
+            {
+                gObjects[i].SetActive(true);
+            }
+        }
+    }
+
+}
