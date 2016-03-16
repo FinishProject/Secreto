560605798b3973683ae3a44484dcce12c247f7c2
LeeRang <asw77id@gmail.com>
2016-03-16 18:32:15 +0900

    고래 이동 수정
    고래 이동 타입 수정,
    화면 밖 이동 불가, 일정 시간 동안만 이동 가능

--- Assets/02. Script/Player/WahleCtrl.cs
+++ Assets/02. Script/Player/WahleCtrl.cs
@@ -3,64 +3,82 @@ using System.Collections;
 
 public class WahleCtrl : MonoBehaviour {
 
+    private enum Type { trace, mouse, keybord };
+
+    private Type moveType = Type.trace;
+
     public float speed; // 이동속도
     public static bool isChange = true; // 스위칭 체크
-    private bool isType = false; // 추격 방법 체크
+    private bool isFush;
+
+    private float countTime = 0f; 
+    public float maxTime = 5f; 
 
     public Transform playerTr;
     private Vector3 moveDir;
 
     private GameObject targetObj = null;
-    private bool isFush;
+    private Vector3 camVec;
 
     void FixedUpdate()
     {
+        Debug.Log(countTime);
         //이동 방식 스위칭
-        if (Input.GetKeyDown(KeyCode.Tab)) { isChange = !isChange; }
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            isChange = !isChange;
+            countTime = 0f;
+            if (!isChange) { moveType = Type.keybord;}
+            else if (isChange) { moveType = Type.trace; }
+        }
+        else if (Input.GetMouseButton(1) && isChange) {
+            moveType = Type.mouse;
+            GetMousePos();
+        }
+        CheckOutCamera();
+        MoveType();
 
-        //캐릭터 조종
-        if (isChange)
-        {
-            float distance = Vector3.Distance(new Vector3(moveDir.x, moveDir.y, 0f),
-                new Vector3(transform.position.x, transform.position.y, 0f));
-            //캐릭터를 추격함 (클릭한 위치이고 플레이어가 이동 시)
-            if (distance <= 3f && PlayerCtrl.instance.inputAxis != 0.0f) { isType = false; }
-            //클릭한 마우스 위치로 이동함
-            if (Input.GetMouseButton(1))
-            {
-                isType = true;
-                GetMousePos();
-            }
-            MoveType(); //이동
-        }
-        //고래 조종
-        else if (!isChange)
-        {
-            moveDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-            transform.Translate(moveDir * (speed * 10f) * Time.deltaTime);
-        }
         //키 입려에 따른 척력 인력 실행
         if (Input.GetKey(KeyCode.V)) { FullFushObject(); isFush = true; }
         else if (Input.GetKey(KeyCode.C)) { FullFushObject(); isFush = false; }
         else { StopCoroutine("GrabObject"); targetObj = null; } // 잡기 중지
     }
+    //카메라 밖 체크
+    void CheckOutCamera()
+    {
+        camVec = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (camVec.x >= Camera.main.pixelWidth || camVec.x <= -1f) moveType = Type.trace;
+        else if (camVec.y >= Camera.main.pixelHeight) moveType = Type.trace;
+    }
     //고래 이동 타입
     void MoveType()
     {
-        //플레이어 추격
-        if (!isType)
+        switch (moveType)
         {
-            transform.position = Vector3.Lerp(transform.position,
+            case Type.trace:
+                transform.position = Vector3.Lerp(transform.position,
                     playerTr.position - (playerTr.forward * 1.0f) + (playerTr.up * 1.5f),
                     speed * Time.deltaTime);
-        }
-        //마우스 위치 추격
-        else if (isType)
-        {
-            transform.position = Vector3.Lerp(transform.position,
-                new Vector3(moveDir.x, moveDir.y, 0), speed * Time.deltaTime);
+                break;
+            case Type.mouse:
+                transform.position = Vector3.Lerp(transform.position,
+                        new Vector3(moveDir.x, moveDir.y, 0), speed * Time.deltaTime);
+                CountDonw();
+                break;
+            case Type.keybord:
+                moveDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+                transform.Translate(moveDir * (speed * 10f) * Time.deltaTime);
+                CountDonw();
+                break;
         }
     }
+
+    void CountDonw()
+    {
+        countTime += Time.deltaTime;
+        if (countTime >= maxTime) { moveType = Type.trace; isChange = true; }
+    }
+
     //마우스 좌표값 구하기
     void GetMousePos()
     {
