cd6df6f5e5b4244732abbfc6ffd40ae60dbc1758
LeeRang <asw77id@gmail.com>
2016-03-22 17:26:18 +0900

    날씨에 반응하는 오브젝트 추가

--- Assets/02. Script/System/ObjectForWeather.cs
+++ Assets/02. Script/System/ObjectForWeather.cs
@@ -3,35 +3,46 @@ using System.Collections;
 
 public class ObjectForWeather : MonoBehaviour, WorldObserver
 {
-    WorldSubject worldData;
-    WeatherState weatherState;
-    float weatherValue;
+    public bool isInfluence_Wind;
+    public bool isInfluence_Rain;
 
+    private WorldSubject worldData;
+    private WeatherState weatherState;
+    private float weatherValue;
     private Transform tr;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         tr = GetComponent<Transform>();
 
         // 옵저버 등록
         worldData = WorldCtrl.GetInstance().RetrunThis();
         worldData.registerObserver(this);
     }
-	
-	// Update is called once per frame
-	void FixedUpdate() {
 
-        if ((WeatherState.NONE & weatherState) == WeatherState.NONE)
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // 날씨 없음
+        if ((WeatherState.NONE & weatherState) != 0)
         {
 
         }
-        if ((WeatherState.WIND_LR & weatherState) == WeatherState.WIND_LR)
+
+        // 바람 _ 좌우
+        if ((WeatherState.WIND_LR & weatherState) != 0 && isInfluence_Wind)
         {
             tr.position = Vector3.Lerp(tr.position, tr.position - (Vector3.right * weatherValue), Time.deltaTime);
         }
-        if ((WeatherState.RAIN & weatherState) == WeatherState.RAIN)
-        {
 
+        // 비
+        if ((WeatherState.RAIN & weatherState) != 0 && isInfluence_Rain)
+        {
+            Vector3 tempScale = tr.transform.localScale;
+            Vector3 tempPos = tr.transform.position;
+            tempScale.y += 0.02f;
+            tr.transform.localScale = tempScale;
         }
     }
 
