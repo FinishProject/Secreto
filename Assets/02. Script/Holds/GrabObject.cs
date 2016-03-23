adbb1867b969d496d84029cde235e075f5709a42
LeeRang <asw77id@gmail.com>
2016-03-23 11:25:46 +0900

    저울, 장애물
    저울 수정,
    플레이어를 추격하는 오브젝트 추가,
    아래로 내려오는 벽 오브젝트 추가

---
+++ Assets/02. Script/Holds/GrabObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabObject : MonoBehaviour {
+
+    private Transform playerTr;
+
+	void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                playerTr = col.GetComponent<Transform>();
+                Vector3 relativePos = playerTr.position - transform.position;
+                transform.position = Vector3.Lerp(transform.position, 
+                    new Vector3(playerTr.position.x + 1f, playerTr.position.y + 3f, 0f),
+                    50f * Time.deltaTime);
+            }
+        }
+    }
+}
