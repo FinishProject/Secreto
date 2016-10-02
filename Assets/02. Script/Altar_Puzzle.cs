c59ec884ccfef29e76f90a97d3c3bfb21c5180ce
LeeRang <asw77id@gmail.com>
2016-10-02 21:48:16 +0900

    11

--- Assets/02. Script/Altar_Puzzle.cs
+++ Assets/02. Script/Altar_Puzzle.cs
@@ -5,14 +5,17 @@ public class Altar_Puzzle : MonoBehaviour {
 
     public GameObject[] stepHolds;
 
-    void OnCollisionStay(Collision col)
+    private float alpha = 1f;
+
+    void OnCollisionEnter(Collision col)
     {
         if (col.collider.CompareTag("OBJECT"))
         {
-            for(int i=0; i<stepHolds.Length; i++)
+            for (int i = 0; i < stepHolds.Length; i++)
             {
                 stepHolds[i].SetActive(true);
             }
+            StartCoroutine(FadeColor(1));
         }
     }
 
@@ -20,10 +23,40 @@ public class Altar_Puzzle : MonoBehaviour {
     {
         if (col.collider.CompareTag("OBJECT"))
         {
-            for (int i = 0; i < stepHolds.Length; i++)
+            
+            StartCoroutine(FadeColor(-1));
+        }
+    }
+
+    IEnumerator FadeColor(float fadeDir)
+    {
+        Color[] color = new Color[stepHolds.Length];
+
+        for(int i=0; i<color.Length; i++)
+        {
+            color[i] = stepHolds[i].GetComponent<Renderer>().material.color;
+        }
+        Debug.Log("11");
+        while (true)
+        {
+            alpha += fadeDir * 1f * Time.deltaTime;
+            alpha = Mathf.Clamp01(alpha);
+
+            for (int i = 0; i < color.Length; i++)
             {
-                stepHolds[i].SetActive(false);
+                color[i].a = alpha;
+                stepHolds[i].GetComponent<Renderer>().material.color = color[i];
             }
+
+            if (alpha <= 0f || alpha >= 1f)
+                break;
+
+            yield return null;
+        }
+        if (fadeDir.Equals(-1))
+        {
+            for (int i = 0; i < stepHolds.Length; i++)
+                stepHolds[i].SetActive(false);
         }
     }
 }
